77f395c555370325b3a3e9cd7314d2c79272acba
Lalindran <lalindran@pandahub.com>
2025-09-01 20:52:49 -0400

    Add Product Sorting test case

---
+++ tests/sorting.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from "@playwright/test";
+import { LoginPage } from "../pages/LoginPage";
+import { InventoryPage } from "../pages/InventoryPage";
+import * as data from "../utils/testData.json";
+
+test("Sort products by Price Low to High", async({page})=>{
+
+
+    const loginPage = new LoginPage(page);
+    const inventoryPage = new InventoryPage(page);
+
+    await loginPage.goto('/');
+    await loginPage.login(process.env.USERNAME!, process.env.PASSWORD!);
+
+    await inventoryPage.sortLowToHigh();
+    const prices = await inventoryPage.getAllPrices();
+
+    const sorted = [...prices].sort((a,b) => a-b);
+    expect(prices).toEqual(sorted);
+
+})
